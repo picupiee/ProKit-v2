88cee1084ab5aab44510b4a351f2a1347bd16a8d
picupiee <picupiee.id@gmail.com>
2024-12-09 01:57:26 +0700

    First styling

--- app/index.tsx
+++ app/index.tsx
@@ -3,14 +3,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello</Text>
+    <View>
+      <AuthScreen />
     </View>
   );
 }
--- components/screens/AuthScreen.tsx
+++ components/screens/AuthScreen.tsx
@@ -1,6 +1,6 @@
 import { Link } from "expo-router";
 import React, { useState } from "react";
-import { Button, Text, View, TextInput } from "react-native";
+import { Button, Text, View, TextInput, TouchableOpacity } from "react-native";
 import { auth } from "../../src/utils/firebase";
 import {
   signInWithEmailAndPassword,
@@ -38,10 +38,29 @@ const AuthScreen = () => {
   };
 
   return (
-    <View>
-      <View>
-        <Button title="Login" onPress={handleLoginClick} />
-        <Button title="Register" onPress={handleRegisterClick} />
+    <View className="flex-1 justify-center items-center px-4 bg-gray-100">
+      <Text>Welcome to ProKit</Text>
+      <View className="flex-row justify-center space-x-4">
+        <TouchableOpacity
+          className={`px-4 py-2 rounded ${
+            !isRegistering
+              ? "bg-blue-500 text-white"
+              : "bg-gray-200 text-gray-600"
+          }`}
+          onPress={handleLoginClick}
+        >
+          <Text>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className={`px-4 py-2 rounded ${
+            !isRegistering
+              ? "bg-blue-500 text-white"
+              : "bg-gray-200 text-gray-600"
+          }`}
+          onPress={handleRegisterClick}
+        >
+          <Text>Register</Text>
+        </TouchableOpacity>
       </View>
 
       {isRegistering ? (
@@ -50,18 +69,21 @@ const AuthScreen = () => {
             placeholder="Email"
             value={email}
             onChangeText={setEmail}
+            className="border border-gray-300 px-4 py-2 rounded mt-4 mb-2 placeholder-gray-400"
           />
           <TextInput
             placeholder="Password"
             secureTextEntry
             value={password}
             onChangeText={setPassword}
+            className="border border-gray-300 px-4 py-2 rounded mt-4 mb-2 placeholder-gray-400"
           />
           <TextInput
             placeholder="Confirm Password"
             secureTextEntry
             value={password}
             onChangeText={setPassword}
+            className="border border-gray-300 px-4 py-2 rounded mt-4 mb-2 placeholder-gray-400"
           />
           <Button title="Submit" onPress={handleRegister} />
         </View>
@@ -72,14 +94,21 @@ const AuthScreen = () => {
             placeholder="Email"
             value={email}
             onChangeText={setEmail}
+            className="border border-gray-300 px-4 py-2 rounded mt-4 mb-2 placeholder-gray-400"
           />
           <TextInput
             placeholder="Password"
             secureTextEntry
             value={password}
             onChangeText={setPassword}
+            className="border border-gray-300 px-4 py-2 rounded mt-4 mb-2 placeholder-gray-400"
           />
-          <Button title="Login" onPress={handleLogin} />
+          <TouchableOpacity
+            className="bg-blue-500 text-white px-4 py-2 rounded mt-4 w-full"
+            onPress={handleLogin} // or handleRegister, depending on the form
+          >
+            <Text className="text-center">Login</Text>
+          </TouchableOpacity>
         </View>
       )}
     </View>
